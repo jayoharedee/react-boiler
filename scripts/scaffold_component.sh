6d84ccc703e1fa180b880b6206800351c25e5607
Jordan Michael <jordan@caxap.design>
2019-10-31 02:16:29 -0400

    a bit more robust

--- scripts/scaffold_component.sh
+++ scripts/scaffold_component.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 ] ; then
-  echo "Please make sure your provide a component name."
+  echo "Please make sure your provide a component name." && exit 1
 fi
 
 DIR=$(basename "`pwd`" )
@@ -14,12 +14,18 @@ case $DIR in
     ;;
   js)
     cp -rf ./components/example ./components/$1
+    sed -i -e "s/Example/$1/g" .components/$1/index.js .components/$1/Example.js
+    mv ./components/$1/Example.js ./components/$1/$1.js
     ;;
-  src)
+  src)np
     cp -rf ./js/components/example ./js/components/$1
+    sed -i -e "s/Example/$1/g" ./js/components/$1/index.js ./js/components/$1/Example.js
+    mv ./js/components/$1/Example.js ./js/components/$1/$1.js
     ;;
   react-boiler)
     cp -rf ./src/js/components/example ./src/js/components/$1
+    sed -i -e "s/Example/$1/g" ./src/js/components/$1/index.js ./src/js/components/$1/Example.js
+    mv ./src/js/components/$1/Example.js ./src/js/components/$1/$1.js
     ;;
   *)
     echo "Something went awry. Make sure your in root or a child of src."
