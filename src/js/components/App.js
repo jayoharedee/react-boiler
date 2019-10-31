ea4c9d8ff5dea1b5d405f27ba9f843b4b8ef43d3
Jordan Michael <jordan@caxap.design>
2019-10-30 22:39:45 -0400

    adjusted routes

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,14 +3,14 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
-import example from './example'
+import Example from './example'
 
 const App = () => {
   return (
     <Router>
       {/* <TopNav /> */}
-      <Route exact path="/" component={example} />
-      <Route exact path="/example" component={example} />
+      <Route exact path="/" component={Example} />
+      <Route exact path="/example" component={Example} />
       {/* <Route exact path="/about" component={Placeholder} />
       <Route exact path="/studys" component={Placeholder} />
       <Route exact path="/portfolio" component={Placeholder} />
