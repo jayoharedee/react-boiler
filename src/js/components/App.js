5822287adff2999d67599c3a3954217a250f8c2b
Jordan Michael <jordan@caxap.design>
2019-11-16 22:34:53 -0500

    removing comments

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { BrowserRouter as Router } from 'react-router-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Example from './example'
@@ -10,11 +9,6 @@ const App = () => {
       {/* <TopNav /> */}
       <Route exact path="/" component={Example} />
       <Route exact path="/example" component={Example} />
-      {/* <Route exact path="/about" component={Placeholder} />
-      <Route exact path="/studys" component={Placeholder} />
-      <Route exact path="/portfolio" component={Placeholder} />
-      <Route exact path="/awards" component={Placeholder} />
-      <Route exact path="/contact" component={Placeholder} /> */}
     </Router>
   )
 }
